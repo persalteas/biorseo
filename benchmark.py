01291d4d565f813fd079ea48e49a40fc5e8247e7
Lenaic <lenaic7.durand@gmail.com>
2020-07-09 11:36:38 +0200

    stat only

--- benchmark.py
+++ benchmark.py
@@ -1680,7 +1680,8 @@ if __name__ == '__main__':
 				tick.label2.set_rotation(90)
 			else:
 				tick.label2.set_fontsize(12)
-		xpos = [ x for x in range(1,19) ]
+		#xpos = [ x for x in range(1,19) ]
+		xpos = [ x for x in range(20) ]
 		print(len(n), len(xpos))
 		plt.yticks([ 20*x for x in range(3) ])
 		plt.ylim((0,40))
