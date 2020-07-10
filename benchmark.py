0011c1cba2e539950d58b04360141269bb5b2367
Lenaic <lenaic7.durand@gmail.com>
2020-07-10 12:02:21 +0200

    stat only

--- benchmark.py
+++ benchmark.py
@@ -1559,15 +1559,16 @@ if __name__ == '__main__':
 		xpos = [ i for i in range(3,len(x_PK_fully)) ]
 		#print(len(x_PK_fully), len(xpos))
 		vplot = axes[1].violinplot(x_PK_fully[3:], showmeans=False, showmedians=False, showextrema=False, points=len(x_PK_fully[0]), positions=xpos)
+		print(len(vplot['bodies']), len(xpos))
 		for patch, color in zip(vplot['bodies'], colors[4:]):
 			patch.set_facecolor(color)
 			patch.set_edgecolor(color)
 			patch.set_alpha(0.5)
-		quartile1, medians, quartile3 = np.percentile(x_PK_fully, [25, 50, 75], axis=1)
+		quartile1, medians, quartile3 = np.percentile(x_PK_fully, [25, 50, 75], axis=1)[3:]
 		print(len(medians), len(xpos))
-		axes[1].scatter(xpos, medians[3:], marker='o', color='k', s=30, zorder=3)
-		axes[1].vlines(xpos, quartile1[3:], quartile3[3:], color='k', linestyle='-', lw=1)
-		for x, y1, y2 in zip(xpos[3:], quartile1[3:], quartile3[3:]):
+		axes[1].scatter(xpos, medians, marker='o', color='k', s=30, zorder=3)
+		axes[1].vlines(xpos, quartile1, quartile3, color='k', linestyle='-', lw=1)
+		for x, y1, y2 in zip(xpos, quartile1, quartile3):
 			bar1 = Line2D([x-0.1, x+0.1], [y1, y1], color="k", lw=1)
 			bar2 = Line2D([x-0.1, x+0.1], [y2, y2], color="k", lw=1)
 			axes[1].add_line(bar1)
