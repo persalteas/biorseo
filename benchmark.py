675f4818ff35581f779f271cae0a645351018048
Lenaic <lenaic7.durand@gmail.com>
2020-07-22 13:52:08 +0200

    +bp2

--- benchmark.py
+++ benchmark.py
@@ -291,13 +291,13 @@ def launch_BayesPairing(module_type, seq_, header_, basename):
 
 
 def launch_BayesPairing2(module_type, seq_, header_, basename):
-
+	
 	if module_type=="rna3dmotif":
-        BP2_type = "rna3dmotif"
-    else:
-        BP2_type = "3DmotifAtlas_ALL"
+		BP2_type = "rna3dmotif"
+	else:
+		BP2_type = "3DmotifAtlas_ALL"
 
-    cmd = ["python3.7", "parse_sequences.py", "-seq", outputDir+basename+".fa", "-samplesize", "1000", "-d", BP2_type]
+	cmd = ["python3.7", "parse_sequences.py", "-seq", outputDir+basename+".fa", "-samplesize", "1000", "-d", BP2_type]
 
 	logfile = open(runDir + "/log_of_the_run.sh", 'a')
 	logfile.write(" ".join(cmd))
