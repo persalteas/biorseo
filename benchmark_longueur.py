e593fb43328a0d70bd67de08d150c91bc0e435f0
Lenaic <lenaic7.durand@gmail.com>
2020-08-25 15:00:08 +0200

    benchmark_longueur

--- benchmark_longueur.py
+++ benchmark_longueur.py
@@ -33,7 +33,7 @@ while step < len(seq)+50:
 	cmd = ["./bin/biorseo", "-d", "./data/modules/DESC", "-s", "./ZDFS33.fa", "-v"]
 
 	old_time = time.time()
-	output = subprocess.check_output(cmd).decode("utf-8").split("\n")
+	output = subprocess.check_output(cmd).decode("utf-8").split("\n")[-5:]
 	run_time = time.time() - old_time
 
 	print(output)
