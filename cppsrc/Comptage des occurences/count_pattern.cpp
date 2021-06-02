12562124c0cc636ed7bd940fee0986b6c337372e
Nathalie Bernard <nathalie.bernard78@outlook.fr>
2021-06-02 13:29:03 +0200

    Amélioration du count_pattern en utilisant find

--- cppsrc/Comptage des occurences/count_pattern.cpp
+++ cppsrc/Comptage des occurences/count_pattern.cpp
@@ -6,6 +6,7 @@
 #include <set>
 #include <algorithm>
 #include <cstdio>
+#include <vector>
 
 using namespace std;
 using json = nlohmann::json;
@@ -13,7 +14,7 @@ using json = nlohmann::json;
 //Create a new file base on the one in argument that will contains one set of pfams for each pattern
 string pfams_union (const string& jsonfile) {
     std::ifstream lib(jsonfile);
-    string out = "/mnt/c/Users/natha/Documents/IBISC/biorseo2/biorseo/data/modules/ISAURE/test.json";
+    string out = "/mnt/c/Users/natha/Documents/IBISC/biorseo2/biorseo/data/modules/ISAURE/Motifs_version_initiale/test.json";
     std::ofstream outfile (out);
 
     json js = json::parse(lib);
@@ -31,6 +32,7 @@ string pfams_union (const string& jsonfile) {
             set<string> pfams;
             //json js2 = it.value();
             if (!test.compare(keys[2])) {
+                //cout << "id: " << id << endl;
                 vector<vector<string>> tab = it2.value();
                 for (uint k = 0; k < tab.size(); k++) {
                     for (uint l = 0; l < tab[k].size(); l++) {
@@ -49,7 +51,8 @@ string pfams_union (const string& jsonfile) {
                 cout << endl;*/
             } else {
                 new_id[test] = it2.value();
-            }       
+            }  
+     
         }
         new_motif[id] = new_id;
         new_id.clear();
@@ -83,7 +86,7 @@ bool is_contains_set(set<string>& s1, set<string>& s2) {
     return true;
 }
 
-void counting_occurences(const string& jsonfile, const string& jsonoutfile) {
+void counting_occurences2(const string& jsonfile, const string& jsonoutfile) {
     std::ifstream lib(jsonfile);
     std::ifstream lib2(jsonfile);
     
@@ -157,20 +160,242 @@ void counting_occurences(const string& jsonfile, const string& jsonoutfile) {
     outfile.close();
 }
 
+int is_contains(string& seq1, string& seq2) {
+
+    uint size1 = seq1.size();
+    uint size2 = seq2.size();
+    int index = -1;
+    if (size1 > size2) {
+        //cout << "size1: " << size1 << ", size2: " << size2 << endl;
+        return -1;
+    }
+
+    /*cout << "seq1: " << seq1 << endl;
+    cout << "seq2: " << seq2 << endl;*/
+    index = seq2.find(seq1);
+    if (index == -1) {
+        return -1;
+    } else {
+        //cout << "index: " << index << endl;
+        return index;
+    }
+    return -1;
+    
+}
+
+void counting_occurences(const string& jsonfile, const string& jsonoutfile) {
+    std::ifstream lib(jsonfile);
+    std::ifstream lib2(jsonfile);
+    
+    std::ofstream outfile (jsonoutfile);
+    json new_motif;
+    json new_id;
+    string delimiter = "&";
+
+    json js = json::parse(lib);
+    json js2 = json::parse(lib2);
+    
+    for (auto it = js.begin(); it != js.end(); ++it) {
+        string id = it.key();
+        string test;
+        uint count = 0;
+        uint occurrences = 0;
+        int fin;
+        string sequence;
+        string struc;
+        vector<string> composantes;
+        vector<string> tab_struc;
+
+        for (auto it2 = js[id].begin(); it2 != js[id].end(); ++it2) {      
+            test = it2.key();
+            
+            if (!test.compare("sequence")) {
+                //cout << "sequence: " << it2.value() << endl;
+                sequence = it2.value();
+                new_id[test] = it2.value();
+
+                string subseq;
+                while(sequence.find(delimiter) != string::npos) {
+                    fin = sequence.find(delimiter);
+                    
+                    subseq = sequence.substr(0, fin);
+                    sequence = sequence.substr(fin + 1);
+                    composantes.push_back(subseq); // new component sequence
+                    //std::cout << "subseq: " << subseq << endl;
+                } 
+                if (!sequence.empty()) {
+                    composantes.push_back(sequence);
+                    //std::cout << "subseq: " << seq << endl;
+                }
+  
+            } else if (!test.compare("struct2d")) {
+                //cout << "struct2d: " << it2.value() << endl;
+                struc = it2.value();
+                new_id[test] = it2.value();
+                string subseq;
+                while(struc.find(delimiter) != string::npos) {
+                    fin = struc.find(delimiter);
+                    
+                    subseq = struc.substr(0, fin);
+                    struc = struc.substr(fin + 1);
+                    tab_struc.push_back(subseq); // new component sequence
+                    //std::cout << "subseq: " << subseq << endl;
+                } 
+                if (!struc.empty()) {
+                    tab_struc.push_back(struc);
+                    //std::cout << "subseq: " << seq << endl;
+                }
+            }
+             else if (!test.compare("occurences")) {
+                occurrences = it2.value();
+            } else {
+                new_id[test] = it2.value();
+            }  
+        }
+        //cout << "-------begin---------" << endl;
+        
+        for (auto it3 = js2.begin(); it3 != js2.end(); ++it3) {
+            string id2 = it3.key();
+            string sequence2, struc2;
+            vector<string> composantes2;
+            vector<string> tab_struc2;
+            int fin;
+            //cout << "id: " << id << " / id2: " << id2 << endl;
+            for (auto it4 = js[id2].begin(); it4 != js[id2].end(); ++it4) {
+                string test = it4.key();
+                
+                if (id != id2) {
+                    if (!test.compare("sequence")) {
+                        sequence2 = it4.value();
+                        string subseq;
+                        while(sequence2.find(delimiter) != string::npos) {
+                            fin = sequence2.find(delimiter);
+                            
+                            subseq = sequence2.substr(0, fin);
+                            sequence2 = sequence2.substr(fin + 1);
+                            composantes2.push_back(subseq); // new component sequence
+                            //std::cout << "subseq: " << subseq << endl;
+                        } 
+                        if (!sequence2.empty()) {
+                            composantes2.push_back(sequence2);
+                            //std::cout << "subseq: " << seq << endl;
+                        }
+
+                    } else if (!test.compare("struct2d")) {
+                        struc2 = it4.value();
+                        string subseq;
+                        while(struc2.find(delimiter) != string::npos) {
+                            fin = struc2.find(delimiter);
+                            
+                            subseq = struc2.substr(0, fin);
+                            struc2 = struc2.substr(fin + 1);
+                            tab_struc2.push_back(subseq); // new component sequence
+                            //std::cout << "subseq: " << subseq << endl;
+                        } 
+                        if (!struc.empty()) {
+                            tab_struc2.push_back(struc2);
+                            //std::cout << "subseq: " << seq << endl;
+                        }
+                        /*if(id == "1" && id2 == "100006")
+                            for (uint j = 0; j < tab_struc2.size() ; j++) {
+                                cout << "tab_struc2[" << j << "]: " << tab_struc2[j] << endl << endl;
+                                cout << "composantes2[" << j << "]: " << composantes2[j] << endl << endl;
+                            }*/
+                        
+                        uint number = 0;
+                        int tab[composantes.size()];
+                        for (uint k = 0; k < composantes.size() ; k++) {
+                            bool flag = false;
+                            for (uint l = 0; l < composantes2.size(); l++) {
+
+                                int test1 = is_contains(composantes[k], composantes2[l]);
+                                int test2 = is_contains(tab_struc[k], tab_struc2[l]);
+                                /*if (id == "1" && id2 == "100006") {
+                                    
+                                    cout << endl;
+                                }*/
+                                /*if (id == "10" && id2 == "65") {
+                                    cout << "----begin----" << endl;
+                                    cout << "id: " << id << " id2: " << id2 << endl;
+                                    cout << "[" << k << "][" << l << "]: " << endl;
+                                    cout << "test1: " << test1 << " | " << "test2: " << test2 << endl;
+                                    cout << "seq1: " << composantes[k] << " vs " << "seq2: " << composantes2[l] << endl;
+                                    cout << "struc1: " << tab_struc[k] << " vs " << "struc2: " <<tab_struc2[l] << endl << endl;*/
+                                if (test1 == test2 && test1 != -1 && test2 != -1) {
+                                    //cout << "flag: " << flag << endl;
+                                    if(!flag) {
+                                        if (k == 0 || test1 > tab[k-1])
+                                            tab[k] = test1;
+                                            flag = true;
+                                    }    
+                                }
+                                    //cout << "----end----" << endl;
+                                //}
+                            }
+                            if(flag) {
+                               number++;
+                            }
+                        }
+                        if(number == composantes.size()) {
+                            count++;
+                            cout << id << " vs " << id2 << endl;
+                        }
+                        /*int test1 = is_contains(sequence, sequence2);
+                        int test2 = is_contains(struc, struc2);
+                        if (test1 == test2 && test1 != -1 && test2 != -1) {
+                            cout << is_contains(sequence, sequence2) << " / " << is_contains(struc, struc2)<< endl;
+                            count++;
+                            //cout << id << "/" << id2 << ": " << count << endl;
+                        }*/
+                    }
+                }
+            }
+            //cout << endl;
+            /*for (uint j = 0; j < tab_struc2.size() ; j++) {
+                cout << "tab_struc2[" << j << "]: " << tab_struc2[j] << endl << endl;
+            }*/  
+        }
+        new_id["occurences"] = count + occurrences;
+        
+        //cout << "-------ending---------" << endl;
+        new_motif[id] = new_id;
+        new_id.clear();
+        //cout << "valeur: " << ite << endl;
+        /*for (uint i = 0; i < tab_struc.size() ; i++) {
+        cout << "tab_struc[" << i << "]: " << tab_struc[i] << endl << endl;
+        } */
+    }
+
+    outfile << new_motif.dump(4) << endl;
+    outfile.close();
+    
+}
+
 int main()
 {
-    //cout << "------------------BEGIN-----------------" << endl;
+    cout << "------------------BEGIN-----------------" << endl;
     string jsonfile = "/mnt/c/Users/natha/Documents/IBISC/biorseo2/biorseo/data/modules/ISAURE/Motifs_version_initiale/motifs_28-05-2021.json";
-    string out = "/mnt/c/Users/natha/Documents/IBISC/biorseo2/biorseo/data/modules/ISAURE/motifs_pdb_version.json";
+    string out = "/mnt/c/Users/natha/Documents/IBISC/biorseo2/biorseo/data/modules/ISAURE/Motifs_derniere_version/motifs_final.json";
     string tmpfile = pfams_union(jsonfile);
     counting_occurences(tmpfile, out);
 
-    if (std::remove(tmpfile.c_str()) != 0)
+    /*string jsonfile = "/mnt/c/Users/natha/Documents/IBISC/biorseo2/biorseo/data/modules/ISAURE/Motifs_version_initiale/bibli_test.json";
+    string out = "/mnt/c/Users/natha/Documents/IBISC/biorseo2/biorseo/data/modules/ISAURE/Motifs_derniere_version/motifs_final_test.json";
+    string tmpfile = pfams_union(jsonfile);
+    counting_occurences(tmpfile, out);*/
+
+    /*if (std::remove(tmpfile.c_str()) != 0)
 		perror("File deletion failed \n");
 	else
-		cout << "File deleted successfully" << endl;
+		cout << "File deleted successfully" << endl;*/
 
+    /*string s1 = "(..)";
+    string s2 = "((..))";
+    bool test = is_contains(s1, s2);
+    cout << "test: " << test << endl;*/ 
+
+    cout << "------------------END-----------------" << endl;
     return 0;
 
-    //cout << "------------------END-----------------" << endl;
+    
 }
