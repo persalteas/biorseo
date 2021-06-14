5edf7097bcb41889e655893489d9e78937d3154b
Louis Becquey <louis.becquey@univ-evry.fr>
2021-06-14 12:23:20 +0200

    Undone Link renumberings + a few code reformatting

--- cppsrc/biorseo.cpp
+++ cppsrc/biorseo.cpp
@@ -251,7 +251,9 @@ int main(int argc, char* argv[])
 		outfile.close();
 	}
 
-	std::cout << "theta: " << theta_p_threshold << endl;
+	if (verbose)
+		std::cout << "theta: " << theta_p_threshold << endl;
+		
 	/*  QUIT  */
 
 	return EXIT_SUCCESS;
